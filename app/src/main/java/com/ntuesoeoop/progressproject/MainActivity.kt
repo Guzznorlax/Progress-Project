36ec2ea99392864721bc1f73645860af62e86be3
wesley67 <b07505007@ntu.edu.tw>
2020-05-25 00:41:37 +0800

    mt_content

--- app/src/main/java/com/ntuesoeoop/progressproject/MainActivity.kt
+++ app/src/main/java/com/ntuesoeoop/progressproject/MainActivity.kt
@@ -14,7 +14,21 @@ import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 
 
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_first.*
+
+
 class MainActivity : AppCompatActivity() {
+//    private lateinit var progressViewModel: ProgressViewModel
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +38,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
 
+
 //        fab.setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                    .setAction("Action", null).show()
@@ -31,6 +46,7 @@ class MainActivity : AppCompatActivity() {
 //        fab.setOnClickListener {view ->
 //            findNavController(view).navigate(R.id.action_FirstFragment_to_createProgressFragment)
 //        }
+
     }
 
     //make button_view button jump to progress_view
@@ -62,4 +78,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+//    override fun onAttachFragment(fragment: Fragment) {
+//        super.onAttachFragment(fragment)
+//        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view_progress_list)
+//        val adapter = ProgressListAdapter(this)
+//        recyclerView.adapter = adapter
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//
+//        // Get a new or existing ViewModel from the ViewModelProvider.
+//        progressViewModel = ViewModelProvider(this).get(ProgressViewModel::class.java)
+//
+//        progressViewModel.allProgresses.observe(this, Observer { progresses ->
+//            // Update the cached copy of the words in the adapter.
+//            progresses?.let { adapter.setProgress(it) }
+//        })
+//    }
 }
